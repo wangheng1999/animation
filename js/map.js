176230ff7d03115de8d2ce30387ec5b36d074794
wangheng1999 <57803621+wangheng1999@users.noreply.github.com>
2025-02-12 17:10:01 +0800

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -2,3 +2,39 @@ let map_con1_img1 = document.querySelector('.map_con1_img1')
 setTimeout(() => {
     map_con1_img1.style.width = '100%'
 },300)
+
+let map_con4_switch_left = document.querySelector('.map_con4_switch_left')
+let map_con4_switch_right = document.querySelector('.map_con4_switch_right')
+let map_con4_list = document.querySelector('.map_con4_list')
+let map_index = 0
+map_con4_switch_left.onclick = function () {
+    if(map_index > -2){
+        map_index = map_index - 1
+    }else {
+        this.style.cursor = 'not-allowed'
+    }
+    map_con4_list.style.transform = `translateX(${100/3*map_index}%)`
+}
+map_con4_switch_right.onclick = function () {
+    if(map_index < 0){
+        map_index = map_index + 1
+    }else {
+        this.style.cursor = 'not-allowed'
+    }
+    map_con4_list.style.transform = `translateX(${100/3*map_index}%)`
+}
+map_con4_switch_left.onmouseleave = function () {
+    if(map_index == -2){
+        this.style.cursor = 'not-allowed'
+    }else {
+        this.style.cursor = 'pointer'
+    }
+}
+map_con4_switch_right.onmouseleave = function () {
+    if(map_index == 0){
+        this.style.cursor = 'not-allowed'
+    }else {
+        this.style.cursor = 'pointer'
+    }
+}
+
